51f5c838b98f36d376559b29e8ddfc8b7fc14812
Alexis Checura <alexischecura@gmail.com>
2023-02-24 19:02:16 -0300

    fix carrousel movement

--- script.js
+++ script.js
@@ -1,43 +1,74 @@
-const experiencia = document.querySelector('.btn-exp');
-const project = document.querySelector('.btn-project');
-const skills = document.querySelector('.btn-skill');
-const educacion = document.querySelector('.btn-edu');
-const contact = document.querySelector('.btn-contact');
+const container = document.querySelector('.container');
 
-function ocultarInfo() {
-  document.querySelector('.exp').classList.add('no-show')
-  document.querySelector('.carousel').classList.add('no-show')
-  document.querySelector('.logos').classList.add('no-show')
-  document.querySelector('.edu').classList.add('no-show')
-  document.querySelector('.contact').classList.add('no-show')
-}
+container.addEventListener('click', e => {
+  if (e.target.classList.contains('heading-dinamic')) {
+    const classElem = document.querySelector(e.target.dataset.for);
+    classElem.classList?.toggle('no-show');
+    e.target.classList.toggle('selected');
+  }
+});
 
-experiencia.addEventListener('click', () =>{
-  // ocultarInfo();
-  document.querySelector('.exp').classList.toggle('no-show')
-  experiencia.classList.toggle('selected')
-})
+// Funcionalidad Carrucel
+const slidesContainer = document.querySelector('.slides');
+const prevBtn = document.querySelector('.btn-left');
+const nextBtn = document.querySelector('.btn-right');
+const slides = slidesContainer.querySelectorAll('.project');
+const squares = document.querySelector('.squares');
 
-project.addEventListener('click', () =>{
-  // ocultarInfo();
-  document.querySelector('.carousel').classList.toggle('no-show')
-  project.classList.toggle('selected')
-})
+let currentSlide = 0;
+const maxSlides = slides.length;
 
-skills.addEventListener('click', () =>{
-  // ocultarInfo();
-  document.querySelector('.logos').classList.toggle('no-show')
-  skills.classList.toggle('selected')
-})
+const createSquares = function () {
+  slides.forEach((_, i) => {
+    squares.insertAdjacentHTML(
+      'beforeend',
+      `<button class="square" data-slide="${i}">&nbsp;</button>`
+    );
+  });
+};
 
-educacion.addEventListener('click', () =>{
-  // ocultarInfo();
-  document.querySelector('.edu').classList.toggle('no-show')
-  educacion.classList.toggle('selected')
-})
+const activateSquare = function (slide) {
+  document.querySelectorAll('.square').forEach(sqr => {
+    sqr.classList.remove('square--fill');
+  });
+  document
+    .querySelector(`.square[data-slide="${slide}"]`)
+    .classList.add('square--fill');
+};
 
-contact.addEventListener('click', () =>{
-  // ocultarInfo();
-  document.querySelector('.contact').classList.toggle('no-show')
-  contact.classList.toggle('selected')
-})
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${115 * (i - slide)}%)`)
+  );
+
+  activateSquare(slide);
+};
+const init = function () {
+  createSquares();
+  goToSlide(0);
+};
+
+init();
+
+const nextSlide = function () {
+  if (++currentSlide === maxSlides) currentSlide = 0;
+  goToSlide(currentSlide);
+};
+
+const prevSlide = function () {
+  if (--currentSlide < 0) currentSlide = maxSlides - 1;
+  goToSlide(currentSlide);
+};
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
+document.addEventListener('keydown', e => {
+  if (e.key === 'ArrowLeft') prevSlide();
+  if (e.key === 'ArrowRight') nextSlide();
+});
+
+squares.addEventListener('click', e => {
+  if (e.target.classList.contains('square')) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+  }
+});
